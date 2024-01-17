30416b806b971fc977ad088d76f25b3672341437
Bea Sanabria <105684798+Beeatrice@users.noreply.github.com>
2024-01-16 22:03:23 -0400

    Create ListaZero.py

---
+++ ListaZero.py
@@ -0,0 +1,14 @@
+# Criando a lista tarefas
+tarefas = []
+
+# Usando o Input() vamos coletar do usuário qual a tarefa a ser adicionada.
+
+atividade = input('Insira uma atividade: ')
+
+#Adiciona a tarefa indicada pelo usuário a lista de tarefas
+
+tarefas.append(atividade)
+Insira uma atividade: Curtir o video de listas da Hashtag
+
+print(tarefas)
+['Curtir o video de listas da Hashtag']
