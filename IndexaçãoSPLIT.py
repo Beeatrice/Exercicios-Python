72fb65cc8ca947afb4475974c0f9927e957da7b2
Bea Sanabria <105684798+Beeatrice@users.noreply.github.com>
2024-01-21 10:10:28 -0400

    Create IndexaçãoSPLIT.py

---
+++ IndexaçãoSPLIT.py
@@ -0,0 +1,4 @@
+#Dividindo Caractere da String (Manipulando com indexação nova .SPLIT)
+frase = ('Curso em Video Python')
+print(frase.split())
+# Retorna (['Curso', 'Em', 'Video', 'Python']) Retorna uma lista com cada palavra separada por exemplo, .split retorna o index cada palavra recebe indexação nova 
